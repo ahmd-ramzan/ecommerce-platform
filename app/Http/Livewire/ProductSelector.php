bb210ab346f43a8d1500b6fbbe662bfc9b29f2bb
ahmd-ramzan <iamahmdrmzn@gmail.com>
2023-10-30 12:44:51 +0500

    handle prices with variations

--- app/Http/Livewire/ProductSelector.php
+++ app/Http/Livewire/ProductSelector.php
@@ -3,17 +3,37 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use App\Models\Variation;
 use Livewire\Component;
 
 class ProductSelector extends Component
 {
+    protected $listeners = ['skuVariantSelected'];
+
     public Product $product;
 
     public $initialVariation;
 
+    public $skuVariant;
+
     public function mount()
     {
         $this->initialVariation = $this->product->variations->sortBy('order')->groupBy('type')->first();
+    }
+
+    public function skuVariantSelected($variantId)
+    {
+        if (! $variantId) {
+            $this->skuVariant = null;
+            return;
+        }
+
+        $this->skuVariant = Variation::find($variantId);
+    }
+
+    public function addToCart()
+    {
+        
     }
 
     public function render()
--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Cknow\Money\Money;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -10,6 +11,14 @@ use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 class Variation extends Model
 {
     use HasFactory, HasRecursiveRelationships;
+
+    /**
+     * @return Money
+     */
+    public function formattedPrice()
+    {
+        return money($this->price);
+    }
 
     /**
      * @return BelongsTo
