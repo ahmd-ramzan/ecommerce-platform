7592be41a949092c412fa8f28db40ca3892cd263
ahmd-ramzan <iamahmdrmzn@gmail.com>
2023-11-01 16:25:27 +0500

    registring cart service

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Cart\Contracts\CartInterface;
 use App\Models\Category;
 
 class HomeController extends Controller
 {
-    public function __invoke()
+    public function __invoke(CartInterface $cart)
     {
+        dd($cart->create());
         $categories = Category::tree()->get()->toTree();
 
         return view('home', get_defined_vars());
